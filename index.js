8ad36b510ae074a429e7b8605b4521ab07ecfe69
Manikandan R <115456961+Manikandan2005@users.noreply.github.com>
2024-03-20 14:24:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { format } from 'date-fns'
 import path from 'path'
 
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 app.use(express.json())
 
 app.get("/",(req,res)=>{
